4c6e5f9345220c996203415c09855f4c24f3f90b
Yasir <tamalarafat@gmail.com>
2023-11-15 03:49:20 +0100

    Added the shell script to download the SRA files

--- 02_download_SRA_files/02_download_SRA.sh
+++ 02_download_SRA_files/02_download_SRA.sh
@@ -9,11 +9,13 @@
 # -e: output file for standard error
 # -J: job name
 
-bsub -q deptsiantis -n 5 -R "rusage[mem=4280]" -M 300000 -o srao -e srae -J srad
+# Save the bsub command as a variable
+BSUB_CMD="bsub -q deptsiantis -n 5 -R 'rusage[mem=4280]' -M 300000 -o srao -e srae -J srad"
 
-# Download SRA data
-for i in $(cat SRA_accession_IDs.txt); do
-  prefetch ${i}
-done
-
-# End of script
+# Execute the bsub command
+$BSUB_CMD << EOF
+  # Download SRA data
+  for i in \$(cat SRA_accession_IDs.txt); do
+    prefetch \${i}
+  done
+EOF
